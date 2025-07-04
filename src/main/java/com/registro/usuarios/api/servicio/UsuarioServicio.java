c08dd92391dd6b11a83be9522ead2f3ac32589ec
Dav416 <84945215+Dav416@users.noreply.github.com>
2025-07-04 16:18:53 -0500

    feat(Impl): add password encryption and roles mapping

--- src/main/java/com/registro/usuarios/api/servicio/UsuarioServicio.java
+++ src/main/java/com/registro/usuarios/api/servicio/UsuarioServicio.java
@@ -2,9 +2,10 @@ package com.registro.usuarios.api.servicio;
 
 import com.registro.usuarios.api.dto.UsuarioRegistroDTO;
 import com.registro.usuarios.api.modelo.Usuario;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 
-public interface UsuarioServicio {
+public interface UsuarioServicio extends UserDetailsService{
     
     public Usuario guardar(UsuarioRegistroDTO registroDTO);
     
